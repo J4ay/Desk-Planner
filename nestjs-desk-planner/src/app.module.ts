52226a4f6aab7483a334552259ab249aa1ec2aa2
Jay Imort <jayimort2005@gmail.com>
2022-05-12 15:47:50 +0200

    Authentication

--- nestjs-desk-planner/src/app.module.ts
+++ nestjs-desk-planner/src/app.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeskSchema } from './schemas/desk.schema';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/desk_manager'),MongooseModule.forFeature([{ name: 'Desk', schema: DeskSchema }])],
+  imports: [MongooseModule.forRoot('mongodb://localhost/desk_manager'),MongooseModule.forFeature([{ name: 'Desk', schema: DeskSchema }]), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
