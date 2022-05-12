07f24d523d5741cd6512152d3af956fdd5bb876e
Jay Imort <jayimort2005@gmail.com>
2022-05-12 15:51:53 +0200

    Removed junk

--- nestjs-desk-planner/src/app.service.ts
+++ nestjs-desk-planner/src/app.service.ts
@@ -59,13 +59,4 @@ export class AppService {
     return desk;
   }
 
-  private movies: Movie[] = [
-    { id: 1, name: 'Star Wars: The Force Awakens', year: 2015 },
-    { id: 2, name: 'Star Wars: The Last Jedi', year: 2017 },
-    { id: 3, name: 'Star Wars: The Rise of Skywalker', year: 2019 },
-  ];
-
-  getMovies(): Movie[] {
-    return this.movies;
-  }
 }
