b2350018faf99aa03ef6eef2b13f78970935b997
Jay Imort <jayimort2005@gmail.com>
2022-05-17 19:17:43 +0200

    Tryouts

--- react-user-app/src/components/TopAppBar.js
+++ react-user-app/src/components/TopAppBar.js
@@ -5,13 +5,14 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import AppBar from '@mui/material/AppBar';
 import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
+import UserService from '../services/UserService';
 
 const TopAppBar = () => {
   return (
     <Paper sx={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: "20" }} elevation={1}>
     <AppBar position="static" sx = {{ fontSize: 72, bgcolor: '#003366' }}>
       <Toolbar>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} onClick={() => console.dir(UserService.isLoggedIn())}>
           Buchen
           </Typography>
       <IconButton
@@ -20,6 +21,7 @@ const TopAppBar = () => {
           color="inherit"
           aria-label="menu"
           sx={{ mr: 1 }}
+          onClick={() => toggle()}
         >
         <LaptopChromebookIcon  sx={{ fontSize: 32 }} />
         </IconButton>
@@ -30,3 +32,13 @@ const TopAppBar = () => {
 }
 
 export default TopAppBar
+
+
+function toggle(){
+  if (UserService.isLoggedIn()) {
+    UserService.doLogout();
+  }
+  else {
+    UserService.doLogin();
+  }
+}
--- react-user-app/src/index.js
+++ react-user-app/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HttpService from "./services/HttpService";
+import UserService from "./services/UserService";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
+function initUI() {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );}
+  
+  reportWebVitals();
+  UserService.initKeycloak(initUI);
+  HttpService.configure();
+  initUI();
