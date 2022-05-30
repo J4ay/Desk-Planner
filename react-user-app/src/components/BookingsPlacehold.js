777f66e1b11ece8e8f377d7e0ae61de82c87df6a
Darios Pachtsinis <dapait03@hs-esslingen.de>
2022-05-30 10:23:32 +0200

    set BookingCards

--- react-user-app/src/components/BookingsPlacehold.js
+++ react-user-app/src/components/BookingsPlacehold.js
@@ -14,19 +14,44 @@ const BookingsPlacehold = () => {
         sx={{ marginLeft: "12px", bgcolor: "#f4f4f4" }}
       >
         <Grid item>
-          <BookingCard />
+          <BookingCard
+            room="Raum 1 EG"
+            date="Dienstag, 14. Januar 2020"
+            duration="14:00 Uhr - 16:00 Uhr"
+          >
+          </BookingCard>
         </Grid>
         <Grid item>
-          <BookingCard />
+        <BookingCard
+            room="Raum 3 2. OG"
+            date="Donnerstag, 15. Januar 2020"
+            duration="9:30 Uhr - 17:30 Uhr"
+          >
+          </BookingCard>
         </Grid>
         <Grid item>
-          <BookingCard />
+        <BookingCard
+            room="Raum 37 3. OG"
+            date="Freitag, 16. Januar 2020"
+            duration="8:00 Uhr - 10:00 Uhr"
+          >
+          </BookingCard>
         </Grid>
         <Grid item>
-          <BookingCard />
+        <BookingCard
+            room="Raum 17 3. OG"
+            date="Freitag, 16. Januar 2020"
+            duration="11:00 Uhr - 17:00 Uhr"
+          >
+          </BookingCard>
         </Grid>
         <Grid item>
-          <BookingCard />
+        <BookingCard
+            room="Raum 4 EG"
+            date="Montag, 19. Januar 2020"
+            duration="9:00 Uhr - 17:00 Uhr"
+          >
+          </BookingCard>
         </Grid>
       </Grid>
     </Container>
