e7d5ff37f8286325d964bbaaace153f7aca3e21a
Darios Pachtsinis <dapait03@hs-esslingen.de>
2022-06-07 18:12:36 +0200

    Canvas

---
+++ react-user-app/src/components/Canvas.js
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect } from "react";
+
+const Canvas = props => {
+
+    const canvasRef = useRef(null);
+
+    const draw = (ctx) => {
+        //ctx.clearRect(0, 0, ctx.current.width, ctx.current.height);
+        ctx.fillStyle = "blue";
+        ctx.beginPath();
+        ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+        ctx.fill();
+    }
+
+    useEffect(() => {
+    const cvs = canvasRef.current;
+    const ctx = cvs.getContext("2d");
+    let animationFrameId;
+
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    const render = () => {
+        draw(ctx);
+        animationFrameId = window.requestAnimationFrame(render);
+    }
+    render();
+
+    return () => {
+        window.cancelAnimationFrame(animationFrameId);
+    }
+
+    }, [draw]);
+
+    return <canvas ref={canvasRef} {...props}/>
+
+}
+
+export default Canvas;
--- react-user-app/src/components/LayoutDesigner.js
+++ react-user-app/src/components/LayoutDesigner.js
@@ -5,12 +5,13 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import HttpService from "../services/HttpService";
 import BottomNavBar from "./BottomNavBar";
+import Canvas from "./Canvas";
 
 class LayoutDesigner extends React.Component{
 
     render(){
         return(
-            <Container sx={{ marginTop: "64px", marginBottom: "64px"}}>
+            <Container sx={{ marginTop: "64px", marginBottom: "64px", padding: "12px"}}>
                     {/* Hier sind die Buttons zum Einfügen von Elementen*/}
                     <Box tableIcon
                         sx={{ float: "right", display: "flex", alignItems: "left", border: "2px solid black" }}
@@ -34,18 +35,27 @@ class LayoutDesigner extends React.Component{
                         </Button>
                     </Box>
                 <Container>
-                    <Grid container
-                        sx={{ border: "2px solid black", marginLeft: "12px", bgcolor: "#f4f4f4" }}
+                    <Canvas
+                        sx={{ border: "2px solid black", 
+                        marginLeft: "12px", 
+                        marginTop: "64px", 
+                        marginBottom: "12px",
+                        bgcolor: "#f4f4f4",
+                        minHeight: "70%",
+                        height: "50%",
+                        minWidth: "90%",
+                        width: "90%", 
+                        padding: "12px"}}
                     >
 
                     {/* Hier ist der eigentliche Designer*/}
                     Test Container
-                    </Grid>
+                    </Canvas>
                 </Container>
                     {/* Hier ist der Button zum Speichern*/}
                 <Button
                     size="small"
-                    sx={{ marginLeft: "auto", marginRight: "0", zIndex: "1" }}
+                    sx={{ float: "right", marginRight: "0", zIndex: "1", border: "2px solid black" }}
                 >
                     Speichern
                 </Button>
