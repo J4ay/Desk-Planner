e53e3020768288db5d57f0370110a4caa0c00a73
Jay Imort <jayimort2005@gmail.com>
2022-06-24 17:20:25 +0200

    #47 Frankensteins Monster

--- react-user-app/src/components/BookingPlacehold.js
+++ react-user-app/src/components/BookingPlacehold.js
@@ -1,101 +1,10 @@
 import React from "react";
 import Dropdowns from "./Dropdowns";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import HttpService from "../services/HttpService";
 import TimePopUp from "./TimePopUp";
-import UserService from "../services/UserService";
 import { fabric } from "fabric";
-
-async function getLayout(room, can) {
-  let coords = room.roomWallHandles;
-
-  // Create lines from database
-  let lines = [];
-  for (var i = 0; i < 21; i++) {
-    lines[i] = makeLine([
-      coords.x[i] * 0.5 - 50,
-      coords.y[i] * 0.5,
-      coords.x[i + 1] * 0.5 - 50,
-      coords.y[i + 1] * 0.5,
-    ]);
-  }
-  //Connect first and last point in array
-  lines[21] = makeLine([
-    coords.x[0] * 0.5 - 50,
-    coords.y[0] * 0.5,
-    coords.x[19] * 0.5 - 50,
-    coords.y[19] * 0.5,
-  ]);
-
-  let circs = [];
-  //create circles at the end of lines
-  for (var i = 0; i < 21; i++) {
-    circs[i] = makeCircle(coords.x[i], coords.y[i]);
-  }
-
-  for (var i = 0; i < lines.length; i++) {
-    can.add(lines[i]);
-  }
-  for (var i = 0; i < circs.length; i++) {
-    can.add(circs[i]);
-  }
-
-  let desks = room.roomDeskHandles;
-  let rects = [];
-
-  for (var i = 0; i < desks.x.length; i++) {
-    rects[i] = makeRect(
-      desks.x[i] * 0.5 - 50,
-      desks.y[i] * 0.5,
-      i,
-      desks.angle[i]
-    );
-  }
-
-  for (var i = 0; i < rects.length; i++) {
-    can.add(rects[i]);
-  }
-}
-
-function makeRect(x, y, id, angle) {
-  var rect = new fabric.Rect({
-    left: x,
-    top: y,
-    width: 100 * 0.5,
-    height: 60 * 0.5,
-    fill: "rgba(107, 62, 19)",
-    id: id,
-    angle: angle,
-  });
-  return rect;
-}
-
-function makeCircle(x, y) {
-  var c = new fabric.Circle({
-    left: x * 0.5 - 50,
-    top: y * 0.5,
-    strokeWidth: 5,
-    radius: 2,
-    fill: "darkgray",
-    stroke: "darkgray",
-    selectable: false,
-    evented: false,
-  });
-
-  return c;
-}
-
-function makeLine(coords) {
-  return new fabric.Line(coords, {
-    fill: "black",
-    stroke: "black",
-    strokeWidth: 5,
-    selectable: false,
-    evented: false,
-  });
-}
 
 class BookingPlacehold extends React.Component {
   constructor(props) {
@@ -104,15 +13,7 @@ class BookingPlacehold extends React.Component {
   }
 
   componentDidMount() {
-    const openDialog = (id) => {
-      this.setState({ dialogIsOpen: true });
-      this.setState({ id: id });
-    };
-
-    const closeDialog = () => {
-      this.setState({ dialogIsOpen: false });
-    };
-    HttpService.getRoom(1, 1, 1).then((res) => {
+    HttpService.getRoom(1, 4, 4).then((res) => {
       this.setState({ room: res });
     });
 
@@ -129,41 +30,119 @@ class BookingPlacehold extends React.Component {
     }));
     fabric.Object.prototype.originX = fabric.Object.prototype.originY =
       "center";
+    let x = this.openDialog;
+    canvas.on("mouse:down", function (e) {
+      if (e.target) {
+        x(e.target.id);
+        console.log(e.target.id);
+        return;
+      }
+    });
+  }
+  openDialog = (id) => {
+    this.setState({ dialogIsOpen: true });
+    this.setState({ id: id });
+  };
 
-    //############################# display the grid #############################
-    let grid = 50;
-    for (var i = 0; i < 1200 / grid; i++) {
-      canvas.add(
-        new fabric.Line([i * grid, 0, i * grid, 1200], {
-          stroke: "#ccc",
-          selectable: false,
-          evented: false,
-        })
-      );
-      canvas.add(
-        new fabric.Line([0, i * grid, 1200, i * grid], {
-          stroke: "#ccc",
-          selectable: false,
-          evented: false,
-        })
+  closeDialog = () => {
+    this.setState({ dialogIsOpen: false });
+  };
+
+  getLayout = (room, can) => {
+    can.clear();
+    let coords = room.roomWallHandles;
+
+    // Create lines from database
+    let lines = [];
+    for (let i = 0; i < 21; i++) {
+      lines[i] = this.makeLine([
+        coords.x[i] * 0.5 - 50,
+        coords.y[i] * 0.5,
+        coords.x[i + 1] * 0.5 - 50,
+        coords.y[i + 1] * 0.5,
+      ]);
+    }
+    //Connect first and last point in array
+    lines[21] = this.makeLine([
+      coords.x[0] * 0.5 - 50,
+      coords.y[0] * 0.5,
+      coords.x[19] * 0.5 - 50,
+      coords.y[19] * 0.5,
+    ]);
+
+    let circs = [];
+    //create circles at the end of lines
+    for (let i = 0; i < 21; i++) {
+      circs[i] = this.makeCircle(coords.x[i], coords.y[i]);
+    }
+
+    for (let i = 0; i < lines.length; i++) {
+      can.add(lines[i]);
+    }
+    for (let i = 0; i < circs.length; i++) {
+      can.add(circs[i]);
+    }
+
+    let desks = room.roomDeskHandles;
+    let rects = [];
+
+    for (let i = 0; i < desks.x.length; i++) {
+      rects[i] = this.makeRect(
+        desks.x[i] * 0.5 - 50,
+        desks.y[i] * 0.5,
+        i,
+        desks.angle[i]
       );
     }
 
-    canvas.on("mouse:down", function (event) {
-      let target = event.target;
-      if (target.type === "rect") {
-        console.log("AAA");
-        openDialog(target.id);
-        return;
-      }
-      // this.openDialog(table.id);
-      // console.log(UserService.getToken());
-      // HttpService.occupyTable(table.id).then(res => {
-      //   table.occupied = !table.occupied;
-      //   this.forceUpdate();
-      // });
+    for (let i = 0; i < rects.length; i++) {
+      can.add(rects[i]);
+    }
+
+    if (document.getElementsByClassName("upper-canvas")[1])
+      document.getElementsByClassName("upper-canvas")[1].remove();
+  };
+
+  makeRect = (x, y, id, angle) => {
+    let rect = new fabric.Rect({
+      left: x,
+      top: y,
+      width: 100 * 0.5,
+      height: 60 * 0.5,
+      fill: "rgba(107, 62, 19)",
+      id: id,
+      angle: angle,
+      selectable: false,
+      evented: true,
+      hoverCursor: "pointer",
     });
-  }
+    return rect;
+  };
+
+  makeCircle = (x, y) => {
+    let c = new fabric.Circle({
+      left: x * 0.5 - 50,
+      top: y * 0.5,
+      strokeWidth: 2,
+      radius: 1.3,
+      fill: "black",
+      stroke: "black",
+      selectable: false,
+      evented: false,
+    });
+
+    return c;
+  };
+
+  makeLine = (coords) => {
+    return new fabric.Line(coords, {
+      fill: "black",
+      stroke: "black",
+      strokeWidth: 5,
+      selectable: false,
+      evented: false,
+    });
+  };
 
   render() {
     return (
@@ -172,8 +151,7 @@ class BookingPlacehold extends React.Component {
           <Dropdowns />
           <button
             onClick={() => {
-              getLayout(this.state.room, this.__canvas);
-              console.log(UserService.getUsername());
+              this.getLayout(this.state.room, this.__canvas);
             }}
           >
             Room 1
@@ -186,29 +164,9 @@ class BookingPlacehold extends React.Component {
             height: "50%",
             minWidth: "90%",
             width: "90%",
-            border: "2px solid black",
+            //border: "2px solid black",
           }}
         >
-          {/* {this.state.tables.map((table) => {
-          return <Box id={table.id} key={table.id} className="table"           
-          sx={{
-            bgcolor: color(table.occupied),
-            marginTop: "15px",
-            position: "relative",
-            height: "40px",
-            width: "40px",
-            border: "2px solid navy",
-          }}
-          onClick={() => {
-            this.openDialog(table.id);
-            console.log(UserService.getToken());
-            HttpService.occupyTable(table.id).then(res => {
-              table.occupied = !table.occupied;
-              this.forceUpdate();
-            });
-          }}
-          />;
-        })} */}
           <canvas id="c" />
         </Container>
         <TimePopUp
